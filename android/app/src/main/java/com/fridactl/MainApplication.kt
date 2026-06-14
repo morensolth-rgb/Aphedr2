12050b47c6d8995713866885630395e21ff5fa5f
morensolth-rgb <morensolth@gmail.com>
2026-06-14 02:46:39 -0700

    Update MainApplication.kt

--- android/app/src/main/java/com/fridactl/MainApplication.kt
+++ android/app/src/main/java/com/fridactl/MainApplication.kt
@@ -5,31 +5,30 @@ import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
-    private val mReactNativeHost = object : ReactNativeHost(this) {
+    override val reactNativeHost: ReactNativeHost =
+        object : DefaultReactNativeHost(this) {
 
-        override fun getUseDeveloperSupport(): Boolean {
-            return false
+            override fun getUseDeveloperSupport(): Boolean = false
+
+            override fun getPackages(): List<ReactPackage> {
+                return PackageList(this).packages
+            }
+
+            override fun getJSMainModuleName(): String = "index"
         }
-
-        override fun getPackages(): List<ReactPackage> {
-            return PackageList(this).packages
-        }
-
-        override fun getJSMainModuleName(): String {
-            return "index"
-        }
-    }
-
-    override fun getReactNativeHost(): ReactNativeHost {
-        return mReactNativeHost
-    }
 
     override fun onCreate() {
         super.onCreate()
         SoLoader.init(this, false)
+
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            load()
+        }
     }
 }
