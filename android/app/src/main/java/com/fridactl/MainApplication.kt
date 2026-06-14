bb227e6db6cf7ed296942414c7ebf893b7bc4efb
morensolth-rgb <morensolth@gmail.com>
2026-06-14 02:26:13 -0700

    Update MainApplication.kt

--- android/app/src/main/java/com/fridactl/MainApplication.kt
+++ android/app/src/main/java/com/fridactl/MainApplication.kt
@@ -5,30 +5,30 @@ import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
-import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
-    override val reactNativeHost: ReactNativeHost =
-        object : DefaultReactNativeHost(this) {
-            override fun getPackages(): List<ReactPackage> =
-                PackageList(this).packages.apply {
-                    add(RootBridgePackage())
-                }
+    private val mReactNativeHost = object : ReactNativeHost(this) {
 
-            override fun getJSMainModuleName(): String = "index"
-            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+        override fun getUseDeveloperSupport() = false
+
+        override fun getPackages(): List<ReactPackage> {
+            val packages = PackageList(this).packages.toMutableList()
+            return packages
         }
+
+        override fun getJSMainModuleName() = "index"
+
+        override fun getBundleAssetName() = "index.android.bundle"
+    }
+
+    override fun getReactNativeHost(): ReactNativeHost {
+        return mReactNativeHost
+    }
 
     override fun onCreate() {
         super.onCreate()
         SoLoader.init(this, false)
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            load()
-        }
     }
 }
